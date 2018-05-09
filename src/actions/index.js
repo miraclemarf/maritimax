355d137a43cb8cc84d2464c8cb52c3c6279b43e4
marfycarlo <marfycarlo@github.com>
2018-05-10 01:50:09 +0700

    using dynamic host name

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,9 @@ export const FETCH_NEWS = 'fetch_news';
 export const FETCH_MORENEWS = 'fetch_morenews';
 export const FETCH_PRODUCTS = 'fetch_products';
 export const SEARCH_PRODUCTS = 'search_products';
-const BASE_API = 'http://127.0.0.1:3001';
+
+const HOST_NAME = window && window.location && window.location.hostname;
+const BASE_API = `http://${HOST_NAME}:3001`;
 const PROD_API = 'http://siapayangnanya.com/api'
 
 export function fetch_news() {
