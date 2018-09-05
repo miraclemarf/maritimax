1618c047cabf67894f18217d7480b91079cfa5ca
marfycarlo <marfycarlo@github.com>
2018-09-05 15:42:52 +0700

    wording why us

--- src/components/fragment/promo/index.js
+++ src/components/fragment/promo/index.js
@@ -1,15 +1,15 @@
 import { Container, Divider, Header, Image } from 'semantic-ui-react'
 const Promo = (props) => (
   <Container>
-      <Header as='h1' content='Why Maritimax' inverted />
-      <Divider hidden />
-      <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Warnem%C3%BCnde_LPG-Tanker_%2801%29_2006-09-21.JPG/1200px-Warnem%C3%BCnde_LPG-Tanker_%2801%29_2006-09-21.JPG' fluid />
-      <Divider hidden />
-      <p style={{
-    'font-size': '1.2em',
-    'color': '#fff'
-  }}>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, vero nisi velit mollitia nostrum. Sunt error facilis aspernatur sit reiciendis numquam, ipsam. Maiores saepe consectetur facere quod fugiat officia sint. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, vero nisi velit mollitia nostrum. Sunt error facilis aspernatur sit reiciendis numquam, ipsam. Maiores saepe consectetur facere quod fugiat officia sint.
+    <Header as='h1' content='Why Maritimax' inverted />
+    <Divider hidden />
+    <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Warnem%C3%BCnde_LPG-Tanker_%2801%29_2006-09-21.JPG/1200px-Warnem%C3%BCnde_LPG-Tanker_%2801%29_2006-09-21.JPG' fluid />
+    <Divider hidden />
+    <p style={{
+      'font-size': '1.2em',
+      'color': '#fff'
+    }}>
+      Maritimax adalah sebuah online media store partner bagi stakeholder perkapalan, meningkatkan awareness, membantu user mengevaluasi value proportion yang ditawarkan, dan mengedukasi cara-cara sewa dan jual-beli kapal. Maritimax menghubungkan pemilik dan penyewa atau penjual dan pembeli secara langsung tanpa broker, sekaligus menjadi media penghubung jangka panjang antar komunitas pemilik, penyewa, penjual, dan calon pembeli kapal.
       </p>
   </Container>
 );
